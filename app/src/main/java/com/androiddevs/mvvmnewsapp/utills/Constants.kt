ee72beaf0a5624a0eb1be874f08532c1b75827ba
TaqaDo <osmonov1337@gmail.com>
2020-12-30 19:51:11 +0600

    i

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utills/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utills
+
+class Constants {
+    companion object {
+        const val API_KEY = "575810cd32b9452c9c3a670fce22d99b"
+        const val BASE_URL = "http://newsapi.org"
+    }
+}
